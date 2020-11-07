0109d4f85e54f94fba41bc5229ced7fd59f918e6
Trevor Delamorandiere <tdelam@gmail.com>
2020-11-07 10:14:03 -0500

    Initial commit with some set up and config

---
+++ server/db/db.sql
@@ -0,0 +1,14 @@
+/* Contractors table */
+CREATE TABLE contractors (
+    id SERIAL NOT NULL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    location VARCHAR(50) NOT NULL,
+    price_range INT NOT NULL check(price_range >= 1 and price_range <= 5)
+);
+
+/* Seed some data */
+INSERT INTO contractors (name, location, price_range) VALUES ('ACME Inc', 'Toronto', 3);
+INSERT INTO contractors (name, location, price_range) VALUES ('Chicken Inc', 'Toronto', 1);
+INSERT INTO contractors (name, location, price_range) VALUES ('Hammers Inc', 'Toronto', 3);
+INSERT INTO contractors (name, location, price_range) VALUES ('Builders Inc', 'Toronto', 2);
+INSERT INTO contractors (name, location, price_range) VALUES ('Installers Inc', 'Toronto', 5);
